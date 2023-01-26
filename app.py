539173e3277c0b4d2956f39b4a64dbc264fd971b
Hiskiso <roma123qq@gmail.com>
2023-01-26 21:38:53 +0400

    fix knifex

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 import requests
 import re
+import time
 import datetime
 import websocket
 import threading
 import uuid
-DEBUG_MODE = False
+DEBUG_MODE = True
 
 avalible_url = "knifex.skin"  # Обновляй тут url
-twich_names = ['kaifarik0644', ""]  # Тут названия каналов твича
-accounts = ["2ac9d5d3-f4d9-4b2f-a56a-f913740facaa", ""]  # тут айди
+twich_names = ["", ""]  # Тут названия каналов твича
+accounts = ["526584f8-3aa6-4761-982e-c3d65891066a", ""]  # тут айди
 
 
-def joinGiftex(link):
+def joinGiftex(link, message):
     good_count = 0
+    nickname = ""
+    reegx = [s for s in re.findall(r'display-name=(\w*)', message)]
+    if len(reegx) > 0:
+        nickname = reegx[0]
     print("https://" + avalible_url +
-          '/pages/competitive/' + link)
+          '/pages/competitive/' + link + " " + nickname)
     for meta_data in accounts:
-        request = requests.post("https://" + avalible_url + "/api/user/freebie/joinCompetitive/"+link, headers={
-                                "content-type": "application/json", "meta-data": meta_data, "cookie": "id="+meta_data+";"})
+        request = requests.post("https://" + avalible_url + "/api/user/freebie/joinCompetitive/" + link, headers={
+                                "content-type": "application/json", "meta-data": meta_data, "cookie": "id=" + meta_data+";"})
         if request.status_code == 200:
             response = request.json()
             if response["ok"] != False:
@@ -27,7 +32,6 @@ def joinGiftex(link):
             else:
                 print("[" + str(datetime.datetime.now().time()) + "](" + meta_data[:3] + "..." +
                       meta_data[len(meta_data)-3:len(meta_data)] + ") - \033[31mНе ок((\033[0m")
-                print(request.json())
         else:
             print("[" + str(datetime.datetime.now().time()) + "](" + meta_data[:3] + "..." +
                   meta_data[len(meta_data)-3:len(meta_data)] + ") - \033[31mUUID аккаунта неверный\033[0m")
@@ -44,45 +48,43 @@ def applyMessage(message):
         r'knifex\.\w*/pages/competitive/\d{13}', message)]
     if len(regexp) > 0:
         competitiveId = [s for s in re.findall(r'\d{13}', regexp[0])]
-        joinGiftex(competitiveId[0])
+        joinGiftex(competitiveId[0], message)
         
     if "giftex_emit" in message:
         regexp = [s for s in re.findall(
             r'\"link\":\"\d{13}\"', message)]
         competitiveId = [s for s in re.findall(r'\d{13}', regexp[0])]
-        joinGiftex(competitiveId[0])
+        joinGiftex(competitiveId[0], message)
 
 
 def knifexSoket():
-    print("Knifex started...")
-
-    def set_interval(func, sec):
-        def func_wrapper():
-            set_interval(func, sec)
-            func()
-        t = threading.Timer(sec, func_wrapper)
-        t.start()
-        return t
-
+    print("Knifex start...")
     def on_message(ws, message):
         if DEBUG_MODE:
             print(message)
         applyMessage(message)
 
     def onOpen(ws):
-        def timeout():
-            ws.send("2")
-        set_interval(timeout, 25)
         ws.send('422["join",{"ott":"' + str(uuid.uuid4()) + '"}]')
+        def ping():
+             while True:
+                 time.sleep(25)
+                 ws.send("2")
+                 
+        pingThread = threading.Thread(target=ping)
+        pingThread.start()
 
     def on_close(ws, _, __):
         print("### closed Knife ###")
+        knifexInit()
+        print("### Auto open Knife ###")
 
     def on_error(ws, error):
-        print(error)
-        print("### closed Knife ###")
+        print("////////////////////////// ERROR KNIFE ///////////////////////////")
+        knifexInit()
+        print("### Auto open Knife ###")
 
-    ws = websocket.WebSocketApp("wss://" + avalible_url + ":2053/socket.io/?EIO=3&transport=websocket",
+    ws = websocket.WebSocketApp("wss://" + avalible_url + ":2083/socket.io/?EIO=3&transport=websocket",
                                 on_message=on_message,
                                 on_close=on_close,
                                 on_error=on_error,
@@ -92,14 +94,6 @@ def knifexSoket():
 
 
 def twtchSokets(channelName):
-    def set_interval(func, sec):
-        def func_wrapper():
-            set_interval(func, sec)
-            func()
-        t = threading.Timer(sec, func_wrapper)
-        t.start()
-        return t
-
     print("Twitch " + channelName + " started...")
 
     def on_message(ws, message):
@@ -110,14 +104,19 @@ def twtchSokets(channelName):
             ws.send("PONG")
 
     def onOpen(ws):
-        def ping():
-            ws.send("PING")
-        set_interval(ping, 300)
         ws.send('CAP REQ :twitch.tv/tags twitch.tv/commands')
         ws.send('PASS SCHMOOPIIE')
         ws.send('NICK justinfan10764')
         ws.send('USER justinfan10764 8 * :justinfan10764')
         ws.send('JOIN #' + channelName)
+      
+        def ping():
+             while True:
+                 time.sleep(30)
+                 ws.send("PING")
+
+        pingThread = threading.Thread(target=ping)
+        pingThread.start()
 
     def on_close(ws, _, __):
 
@@ -141,7 +140,10 @@ for channel in twich_names:
     twichThread.start()
 
 
-knifexThread = threading.Thread(target=knifexSoket)
+def knifexInit():
+    knifexThread = threading.Thread(target=knifexSoket)
+    knifexThread.start()
+
+knifexInit()
 
 
-knifexThread.start()
